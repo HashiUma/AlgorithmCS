8a1f6f105d0a97a8708d486d2624a29ad4d8ddc9
YumaHashimoto <hashimoto.umao@gmail.com>
2022-03-06 23:58:47 +0900

    UnionFindTree

---
+++ AlgorithmCS/DataStructure/UnionFindTree.cs
@@ -0,0 +1,58 @@
+﻿namespace AlgorithmCS
+{
+    public class UnionFindTree
+    {
+        private int[] _rank { get; set; }
+        private int[] _parent { get; set; }
+
+        public UnionFindTree() { }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="size"></param>
+        public UnionFindTree(int size)
+        {
+            _rank = new int[size];
+            _parent = new int[size];
+            for (int i = 0; i < size; i++) _makeSet(i);
+
+        }
+        public bool Same(int x, int y) => _findSet(x) == _findSet(y);
+
+        public void Unite(int x, int y)
+        {
+            _link(_findSet(x), _findSet(y));
+        }
+
+        private void _makeSet(int x)
+        {
+            _parent[x] = x;
+            _rank[x] = 0;
+        }
+
+        private int _findSet(int x)
+        {
+            int p = x;
+            while (p != _parent[p])
+            {
+                p = _parent[p];
+            }
+            return p;
+        }
+
+        private void _link(int x, int y)
+        {
+            if (_rank[x] > _rank[y])
+            {
+                _parent[y] = x;
+
+            }
+            else
+            {
+                _parent[x] = y;
+                if (_rank[x] == _rank[y]) _rank[y]++;
+            }
+        }
+    }
+}
